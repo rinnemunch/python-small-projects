4471b803cd6fd793c733671f4e5b950e3eebaab6
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-11 10:09:09 -0400

    feat: add guessing loop with feedback and win condition

--- guess-the-number/main.py
+++ guess-the-number/main.py
@@ -2,4 +2,13 @@ import random
 
 print("Welcome to Guess the Number!")
 number = random.randint(1, 100)
-guess = None
+
+while True:
+  guess = int(input("Enter your guess (1-100): "))
+  if guess == number:
+    print("You got it!")
+    break
+  elif guess < number:
+    print("Too low!")
+  else:
+    print("Too high!")
