8d43e8a2d622f12f74a52124026f2a75282976dc
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-11 10:25:31 -0400

    feat: add guess counter and improved the win message

--- guess-the-number/main.py
+++ guess-the-number/main.py
@@ -2,11 +2,14 @@ import random
 
 print("Welcome to Guess the Number!")
 number = random.randint(1, 100)
+attempts = 0
 
 while True:
   guess = int(input("Enter your guess (1-100): "))
+  attempts += 1
+
   if guess == number:
-    print("You got it!")
+    print(f"You got it in {attempts} tries!")
     break
   elif guess < number:
     print("Too low!")
