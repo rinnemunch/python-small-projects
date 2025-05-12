875049c7880007d2f8a92e4c15ae1f66aba03a43
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-12 11:08:36 -0400

    feat: add % and ** operator handling to calculator

--- calculator/main.py
+++ calculator/main.py
@@ -9,14 +9,14 @@ while True:
     except ValueError:
       print("That is not a valid number. Try again.")
 
-  valid_operators = ["+", "-", "*", "/"]
+  valid_operators = ["+", "-", "*", "/", "%", "**"]
 
   while True:
-    op = input("Enter operator (+, -, *, /): ")
+    op = input("Enter operator (+, -, *, /, %, **): ")
     if op in valid_operators:
       break
     else:
-      print("Invalid operator. Please chosse from +, -, *, /. 2433")
+      print("Invalid operator. Please chosse from +, -, *, /.")
 
   while True:
     try:
@@ -36,6 +36,13 @@ while True:
       print("Error: Cannot divide by zero.")
     else:
       print(f"Result: {num1 / num2}")
+  elif op == "%":
+    if num2 == 0:
+      print("Error: Cannot modulo by zero.")
+    else:
+      print(f"Result: {num1 % num2}")
+  elif op == "**":
+    print(f"Result: {num1 ** num2}")
   else:
     print("Invalid operator.")
 
