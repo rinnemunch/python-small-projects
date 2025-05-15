d7474379838b820358763ee91faa920641f7ad8f
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-15 09:20:33 -0400

    feat: add multiple random jokes with punchlines

--- joke-generator/main.py
+++ joke-generator/main.py
@@ -2,12 +2,23 @@ import time
 import random
 
 def tell_joke():
-  print("Here's a joke for you..\n")
-  time.sleep(1.5)
-  print("Why don't programmers like nature?")
-  time.sleep(2)
-  print("It has too many bug\n")
+  jokes = [
+    ("Why don't programmers like nature?", "It has too many bugs!"),
+    ("Why do Python devs wear glasses?", "Because they can't C!"),
+    ("What's a programmer's favorite hangout place?", "The Foo Bar."),
+    ("Why did the developer go broke?", "Because he used up all his cache!"),
+    ("How many programmers does it take to change a light bulb?", "None, that's a hardware problem!")
+  ]
 
+  setup, punchline = random.choice(jokes)
+
+  print("Here's a joke for you...\n")
+  time.sleep(1.5)
+  print(setup)
+  time.sleep(2)
+  print(punchline + "\n")
+
+#Starting the joke
 if __name__ == "__main__":
   input("Press Enter to hear a joke...")
   tell_joke()
