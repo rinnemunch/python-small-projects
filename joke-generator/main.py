dfa8fddb63c370c59483e5cf66840e5272a1f06b
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-15 09:11:08 -0400

    feat: add basic joke delivery with delay

---
+++ joke-generator/main.py
@@ -0,0 +1,13 @@
+import time
+import random
+
+def tell_joke():
+  print("Here's a joke for you..\n")
+  time.sleep(1.5)
+  print("Why don't programmers like nature?")
+  time.sleep(2)
+  print("It has too many bug\n")
+
+if __name__ == "__main__":
+  input("Press Enter to hear a joke...")
+  tell_joke()
